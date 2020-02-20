8039b432045a1a779031495d4ebe916a6ac613ea
Cícero Matheus <matheuslacerda08@hotmail.com>
2020-02-20 15:22:26 -0300

    Primeiro Commit

---
+++ MensagensRSSF.cpp
@@ -0,0 +1,88 @@
+#include "MensagensRSSF.h"
+
+void conexaoFracasso(String URL, String erro, String tentativa)
+{
+    Serial.print("Erro na requisição para " + URL + ": ");
+    Serial.print(erro + ". Momento: ");
+    Serial.println(tentativa);
+}
+
+void conexaoJsonFracasso(String erro, String tentativa)
+{
+    Serial.print("Erro ao interpretar resposta: ");
+    Serial.print(erro + ". Momento: ");
+    Serial.println(tentativa);
+}
+
+void mostrarIPMAC(IPAddress ip, String mac)
+{
+    Serial.println("");
+    Serial.print("IP: ");
+    Serial.println(ip);
+    Serial.print("MAC: ");
+    Serial.println(mac);
+}
+
+void pedidoNaoAutorizado()
+ {
+    Serial.println("Acesso negado.");
+}
+
+void sucesso() 
+{
+    Serial.println("Operação realizada com sucesso!");
+}
+
+void conectando()
+{
+    Serial.print("Conectando");
+}
+
+void esperar()
+{
+    Serial.print(".");
+}
+
+void fimConexao()
+{
+    Serial.println("Conexão perdida.");
+}
+
+void requisicaoNaoAtendida() 
+{
+    Serial.println("Última requisição NÃO foi atendida.");
+}
+
+void requisicaoAtendida()
+{
+    Serial.println("Última requisição foi atendida.");
+}
+
+void exibirMonitoramento(float temperatura, float umidade, int radiacaoUV, int fumaca, int gasToxico, int fogo)
+{
+    Serial.print("Temperatura: ");
+    Serial.print(temperatura);
+    Serial.println(" °C");
+
+    Serial.print("Umidade: ");
+    Serial.print(umidade);
+    Serial.println(" %");
+
+    Serial.print("Radiação UV: Índice ");
+    Serial.println(radiacaoUV);
+
+    Serial.print("Fumaça: ");
+    Serial.println(fumaca);
+
+    Serial.print("Gás Tóxico: ");
+    Serial.println(gasToxico);
+
+    Serial.print("Fogo: ");
+    Serial.println(fogo);
+    
+}
+
+void novaRequisicaoAtendida()
+{
+    Serial.println("Nova requisição atendida com sucesso.");
+}
---
+++ MensagensRSSF.h
@@ -0,0 +1,19 @@
+#ifndef MENSAGENS_H
+#define MENSAGEN_H
+
+#include<Arduino.h>
+
+void conexaoFracasso(String URL, String erro, String tentativa);
+void conexaoJsonFracasso(String erro, String tentativa);
+void mostrarIPMAC(IPAddress ip, String mac);
+void pedidoNaoAutorizado();
+void requisicaoNaoAtendida();
+void requisicaoAtendida();
+void conectando();
+void esperar();
+void sucesso();
+void fimConexao();
+void exibirMonitoramento(float temperatura, float umidade, int radiacaoUV, int fumaca, int gasToxico, int fogo);
+void novaRequisicaoAtendida();
+
+#endif
